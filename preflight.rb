dc3f53213f25f8fc39e29db4d067c3abbe58ccb3
Stephen Lauck <lauck@chef.io>
2017-03-01 14:07:47 -0500

    initial commit of preflight inspec check for chef-server

---
+++ preflight.rb
@@ -0,0 +1,13 @@
+
+control "preflight" do
+  impact 1.0
+  title "Preflight Chef Server Install Check"
+  desc "Should determine if a system is ready for a Chef Server install"
+
+  # shmmax is hardcoded in Automate embedded cookbooks default attribute file
+  shmmax_command = `sysctl -n kernel.shmmax`.chomp!.to_i
+  describe shmmax_command do
+    it { should be >= 17179869184 }
+  end
+
+end
